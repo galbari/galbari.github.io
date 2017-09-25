a576cc9d85f627fb7e69e2353983547c6174418b
galbari <galbari1985@gmail.com>
2017-09-25 17:33:43 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -1,1 +1,23 @@
-console.log('this is my js file');
+function transitionBanner() {
+    function createOrganicCardObj(cardData) {
+        var container = cardData.container;
+        var card = cardData.boxes[0];
+        return {
+            title: card.title,
+            content: card.textContent,
+            container: container,
+            img: card.video_data.thumbnail
+        };
+    }
+
+    function getCardsData() {
+        var allCards = TRCImpl.boxes;
+        var organicCards = allCards.filter(function(card){return card.mode.indexOf('organic');});
+        return organicCards.map(createOrganicCardObj);
+    }
+
+    var cardsData = getCardsData();
+    console.log(cardsData);
+}
+
+transitionBanner();
