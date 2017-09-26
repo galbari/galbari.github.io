217b32d13fe86a39f68c9043af4a95f91dc444e3
galbari <galbari1985@gmail.com>
2017-09-26 12:31:12 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -30,8 +30,15 @@ function transitionBanner() {
         return cardsData.reduce(function(html, card){
             return html + '<li class="item">' +
                             '<div class="img" style="background-image: url(' + card.img + ')"></div>' +
-                            '<div class="header">Up next:</div>' +
-                            '<div class="content">' + card.content + '</div>' +
+                            '<div class="content-container">' +
+                                '<div class="header">Up next:</div>' +
+                                '<div class="content">' + card.content + '</div>' +
+                                '<div class="arrow">' +
+                                    '<svg height="32" viewBox="0 0 32 32" width="32" ' +
+                                        '<path d="M1412 897q0-27-18-45l-91-91q-18-18-45-18t-45 18l-189 189v-502q0-26-19-45t-45-19h-128q-26 0-45 19t-19 45v502l-189-189q-19-19-45-19t-45 19l-91 91q-18 18-18 45t18 45l362 362 91 91q18 18 45 18t45-18l91-91 362-362q18-18 18-45zm252-1q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/>' +
+                                    '</svg>' +
+                                '</div>' +
+                            '</div>' +
                         '</li>';
         }, '');
     }
@@ -41,14 +48,29 @@ function transitionBanner() {
         var itemsContainer = document.createElement('ul');
         var items = getItmesAsHtmlString(cardsData);
 
-        slider.setAttribute('id', 'tbl-slider');
+        slider.id = 'tbl-slider';
+        slider.className += ' tbl-cards-slider';
         slider.appendChild(itemsContainer);
         itemsContainer.innerHTML = items;
 
         return slider;
     }
 
+    function getStyle() {
+        var styleDiv = document.createElement('div');
+        styleDiv.id = 'tbl-slider-style';
+        styleDiv.innerHTML = '<style>' +
+            '.tbl-cards-slider {width: 525px; height: 130px; overflow: hidden; border-radius: 10px;}' +
+            '.tbl-cards-slider .img {display: inline-block; width: 162px; height: 100%; background-size: cover;}' +
+            '.tbl-cards-slider .content-container {display: inline-block; width: 363px; height: 100%; padding: 15px;}' +
+            '.tbl-cards-slider .header {font-weight: bold; font-size: 16px;}' +
+            '.tbl-cards-slider .content {width: 245px; font-size: 14px; overflow: hidden; text-overflow: ellipsis;}' +
+            '.tbl-cards-slider .arrow {float: right;}' +
+        '</style>';
+    }
+
     var cardsData = getCardsData();
+    var style = getStyle();
     var slider = createSlider(cardsData);
 
     document.body.appendChild(slider);
