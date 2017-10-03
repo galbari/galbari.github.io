88d276e49f747c92f856983602548f76cc8edc14
galbari <galbari1985@gmail.com>
2017-10-03 14:48:02 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -172,6 +172,14 @@ function transitionBanner() {
         var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
         var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
 
+        if ('requestAnimationFrame' in window === false) {
+        window.scroll(0, destinationOffsetToScroll);
+            if (callback) {
+                callback();
+            }
+            return;
+        }
+
         function scroll() {
             var now = 'now' in window.performance ? performance.now() : new Date().getTime();
             var time = Math.min(1, ((now - startTime) / duration));
