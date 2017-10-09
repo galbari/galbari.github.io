aed0c67e92fec9b962b92501aa80f7fd74265a61
galbari <galbari1985@gmail.com>
2017-10-09 11:30:29 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -1,5 +1,5 @@
 function transitionBanner() {
-    var !sliderAlreadyHidden = false;
+    var sliderAlreadyHidden = false;
     var waitNumOfMiliSecondsBeforeRemoving = 10000;
     var arrowSVG = '<svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
                         '<defs></defs>' +
