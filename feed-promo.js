d6dc927a2c26e3a6bacc9cb912c8a83ee21dfaae
galbari <galbari1985@gmail.com>
2017-10-16 11:13:48 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -54,36 +54,36 @@ function feedTeaserSlider() {
 
     function Timer(callback, interval) {
         var timerId, startTime, remaining = 0;
-        var state = 0; //  0 = idle, 1 = running, 2 = paused, 3 = resumed
+        this.state = 0; //  0 = idle, 1 = running, 2 = paused, 3 = resumed
 
         this.pause = function () {
-            if (state != 1) return;
+            if (this.state != 1) return;
 
             remaining = interval - (new Date() - startTime);
             window.clearInterval(timerId);
-            state = 2;
+            this.state = 2;
         };
 
         this.resume = function () {
-            if (state != 2) return;
+            if (this.state != 2) return;
 
-            state = 3;
+            this.state = 3;
             window.setTimeout(this.timeoutCallback, remaining);
         };
 
         this.timeoutCallback = function () {
-            if (state != 3) return;
+            if (this.state != 3) return;
 
             callback();
 
             startTime = new Date();
             timerId = window.setInterval(callback, interval);
-            state = 1;
+            this.state = 1;
         };
 
         startTime = new Date();
         timerId = window.setInterval(callback, interval);
-        state = 1;
+        this.state = 1;
     }
 
     function scrollToDestination(destination, duration, easing, callback) {
@@ -248,7 +248,7 @@ function feedTeaserSlider() {
     }
 
     function shouldHideTeaser() {
-        if (teaserVisibilityCountDown || isFeedVisibleToTheUser()) {
+        if ((teaserVisibilityCountDown && teaserVisibilityCountDown.state === 1) || isFeedVisibleToTheUser()) {
             hideTeaser();
         }
     }
