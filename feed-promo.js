ce4b018e85af91c448265ce8fdf9aa588701e436
galbari <galbari1985@gmail.com>
2017-10-28 11:39:55 +0300

    Update feed-promo.js

--- feed-promo.js
+++ feed-promo.js
@@ -71,8 +71,8 @@ function feedTeaserSlider() {
                             '.tbl-cards-teaser .actionMessage {display: none;}' +                        
                             '.tbl-cards-teaser .card-content-mobile {display: inline;}' +                        
                             '.tbl-cards-teaser .card-content-desktop {display: none;}' + 
-                            '.tbl-cards-teaser .tbl-teaser-closeBtn-wrapper {top: 0; right: 5; width: 40px; height: 100%; z-index: 99;}' +                        
-                            '.tbl-cards-teaser .tbl-teaser-closeBtn {display: block; visibility: visible; opacity: 1; background: none; border: none; box-shadow: none; transition: none;}' +
+                            '.tbl-cards-teaser .tbl-teaser-closeBtn-wrapper {top: 0; right: 5px; width: 40px; height: 100%; z-index: 99;}' +                        
+                            '.tbl-cards-teaser .tbl-teaser-closeBtn {height: 100%; right: 10px; display: block; visibility: visible; opacity: 1; background: none; border: none; box-shadow: none; transition: none;}' +
                             '.tbl-cards-teaser .tbl-teaser-closeBtn svg {height: 100%; fill: #000000;}' +                        
                             '.tbl-cards-teaser .arrow {display: none;}' +
                             '.tbl-cards-teaser .tbl-teaser-closeBtn:hover {background: none;}' +
