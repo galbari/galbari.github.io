7288aeed5e28bc75a08a276b85e3bb72988cb23f
galbari <galbari1985@gmail.com>
2018-01-22 14:18:57 +0200

    Update publisher-hook-2-col.js

--- publisher-hook-2-col.js
+++ publisher-hook-2-col.js
@@ -1,12 +1,17 @@
-var LEFT_CONTAINER_CLASS = 'left-feed-container',
-    RIGHT_CONTAINER_CLASS = 'right-feed-container';
+var LEFT_CONTAINER_CLASS = "left-feed-container",
+	RIGHT_CONTAINER_CLASS = "right-feed-container";
 
-var container = document.getElementById('taboola-container');
-var wrapper = document.createElement('div');
-var rightFeedContainer = document.createElement('div');
+var container = document.getElementById("taboola-container");
+var wrapper = document.createElement("div");
+var rightFeedContainer = document.createElement("div");
 
-container.classList =+ LEFT_CONTAINER_CLASS;
-rightFeedContainer.classList =+ RIGHT_CONTAINER_CLASS;
+function addClass(element, className) {
+	if (el.classList) el.classList.add(className);
+	else el.className += " " + className;
+}
+
+addClass(container, LEFT_CONTAINER_CLASS);
+addClass(rightFeedContainer, RIGHT_CONTAINER_CLASS);
 
 container.parentNode.insertBefore(wrapper, container);
 wrapper.appendChild(container);
