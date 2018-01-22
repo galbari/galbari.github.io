7d7d35b225c7785b86f77652b88c53aa82211b8e
galbari <galbari1985@gmail.com>
2018-01-22 12:36:46 +0200

    Update publisher-hook-2-col.js

--- publisher-hook-2-col.js
+++ publisher-hook-2-col.js
@@ -1,6 +1,7 @@
 var container = document.getElementById('taboola-container');
 var wrapper = document.createElement('div');
+var newContainer = document.createElement('span');
+newContainer.innerHTML = 'boom!'
 container.parentNode.insertBefore(wrapper, container);
 wrapper.appendChild(container);
-
-
+container.parentNode.insertBefore(newContainer, container.nextSibling);
