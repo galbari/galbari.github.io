023986eca88dc0678974b85beebdfabfc34b3764
galbari <galbari1985@gmail.com>
2018-03-27 11:43:34 +0300

    Update publisher-start.js

--- ab-tests/feed-teaser/publisher-start.js
+++ ab-tests/feed-teaser/publisher-start.js
@@ -1,32 +1,34 @@
 TRC.feedTeaserSlider = function() {
-  var options = {};
+	var options = {};
 	var jsSrc =
 		'//s3.amazonaws.com/c3.taboola.com/ui-ab-tests/feed-teaser/cdn-teaser.js';
 	var cssSrc =
-    '//s3.amazonaws.com/c3.taboola.com/ui-ab-tests/feed-teaser/feed-teaser-style.css';
-    
+		'//s3.amazonaws.com/c3.taboola.com/ui-ab-tests/feed-teaser/feed-teaser-style.css';
 
   //check the variant id func
+  loadTeaser(options)
 
-	function loadCSSFile() {
-		var link = document.createElement('link');
-		link.href = cssSrc;
-		link.type = 'text/css';
-		link.rel = 'stylesheet';
-		document.getElementsByTagName('body')[0].appendChild(link);
-  }
-  
 
-	function loadScriptFile() {
-		var teaserScript = document.createElement('script');
-		teaserScript.id = 'teaser-test-script';
-		teaserScript.onload = function() {
-			TRC.executeFeedTeaser(options);
-		};
-		teaserScript.src = jsSrc;
-		document.getElementsByTagName('head')[0].appendChild(teaserScript);
+	function loadTeaser(options) {
+		function loadCSSFile() {
+			var link = document.createElement('link');
+			link.href = cssSrc;
+			link.type = 'text/css';
+			link.rel = 'stylesheet';
+			document.getElementsByTagName('body')[0].appendChild(link);
+		}
+
+		function loadScriptFile() {
+			var teaserScript = document.createElement('script');
+			teaserScript.id = 'teaser-test-script';
+			teaserScript.onload = function() {
+				TRC.executeFeedTeaser(options);
+			};
+			teaserScript.src = jsSrc;
+			document.getElementsByTagName('head')[0].appendChild(teaserScript);
+		}
+
+		loadCSSFile();
+		loadScriptFile();
 	}
-
-	loadCSSFile();
-	loadScriptFile();
 };
